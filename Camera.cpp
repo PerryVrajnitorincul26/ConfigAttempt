e5118ff3773cd4e225f29efb0dd386f2e4267672
Toderica Matei <namelessman26@gmail.com>
2022-12-01 20:15:47 +0200

    My attempt at adding loading an obj file.

---
+++ Camera.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Perry on 11/28/2022.
+//
+
+#include "Camera.h"
--- main.cpp
+++ main.cpp
@@ -3,220 +3,18 @@
 // Lab6 - Texturi.cpp : Defines the entry point for the console application.
 //
 
-#include <stdlib.h> // necesare pentru citirea shader-elor
-#include <stdio.h>
-#include <math.h>
-
-#include <GL/glew.h>
-
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-
-#include <glm/gtc/type_ptr.hpp>
-
-#include <GLFW/glfw3.h>
-
+#include "Camera.h"
+#include "tinyobjTest.h"
 #include <iostream>
 
 #define STB_IMAGE_IMPLEMENTATION
 
 #include <stb_image.h>
 
-
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-enum ECameraMovementType {
-    UNKNOWN,
-    FORWARD,
-    BACKWARD,
-    LEFT,
-    RIGHT,
-    UP,
-    DOWN
-};
-
-class Camera {
-private:
-    // Default camera values
-    const float zNEAR = 0.1f;
-    const float zFAR = 500.f;
-    const float YAW = -90.0f;
-    const float PITCH = 0.0f;
-    const float FOV = 45.0f;
-    glm::vec3 startPosition;
-
-public:
-    Camera(const int width, const int height, const glm::vec3 &position) {
-        startPosition = position;
-        Set(width, height, position);
-    }
-
-    void Set(const int width, const int height, const glm::vec3 &position) {
-        this->isPerspective = true;
-        this->yaw = YAW;
-        this->pitch = PITCH;
-
-        this->FoVy = FOV;
-        this->width = width;
-        this->height = height;
-        this->zNear = zNEAR;
-        this->zFar = zFAR;
-
-        this->worldUp = glm::vec3(0, 1, 0);
-        this->position = position;
-
-        lastX = width / 2.0f;
-        lastY = height / 2.0f;
-        bFirstMouseMove = true;
-
-        UpdateCameraVectors();
-    }
-
-    void Reset(const int width, const int height) {
-        Set(width, height, startPosition);
-    }
-
-    void Reshape(int windowWidth, int windowHeight) {
-        width = windowWidth;
-        height = windowHeight;
-
-        // define the viewport transformation
-        glViewport(0, 0, windowWidth, windowHeight);
-    }
-
-    const glm::mat4 GetViewMatrix() const {
-        // Returns the View Matrix
-        return glm::lookAt(position, position + forward, up);
-    }
-
-    const glm::mat4 GetProjectionMatrix() const {
-        glm::mat4 Proj = glm::mat4(1);
-        if (isPerspective) {
-            float aspectRatio = ((float) (width)) / height;
-            Proj = glm::perspective(glm::radians(FoVy), aspectRatio, zNear, zFar);
-        } else {
-            float scaleFactor = 2000.f;
-            Proj = glm::ortho<float>(
-                    -width / scaleFactor, width / scaleFactor,
-                    -height / scaleFactor, height / scaleFactor, -zFar, zFar);
-        }
-        return Proj;
-    }
-
-    void ProcessKeyboard(ECameraMovementType direction, float deltaTime) {
-        float velocity = (float) (cameraSpeedFactor * deltaTime);
-        switch (direction) {
-            case ECameraMovementType::FORWARD:
-                position += forward * velocity;
-                break;
-            case ECameraMovementType::BACKWARD:
-                position -= forward * velocity;
-                break;
-            case ECameraMovementType::LEFT:
-                position -= right * velocity;
-                break;
-            case ECameraMovementType::RIGHT:
-                position += right * velocity;
-                break;
-            case ECameraMovementType::UP:
-                position += up * velocity;
-                break;
-            case ECameraMovementType::DOWN:
-                position -= up * velocity;
-                break;
-        }
-    }
-
-    void MouseControl(float xPos, float yPos) {
-        if (bFirstMouseMove) {
-            lastX = xPos;
-            lastY = yPos;
-            bFirstMouseMove = false;
-        }
-
-        float xChange = xPos - lastX;
-        float yChange = lastY - yPos;
-        lastX = xPos;
-        lastY = yPos;
-
-        if (fabs(xChange) <= 1e-6 && fabs(yChange) <= 1e-6) {
-            return;
-        }
-        xChange *= mouseSensitivity;
-        yChange *= mouseSensitivity;
-
-        ProcessMouseMovement(xChange, yChange);
-    }
-
-    void ProcessMouseScroll(float yOffset) {
-        if (FoVy >= 1.0f && FoVy <= 90.0f) {
-            FoVy -= yOffset;
-        }
-        if (FoVy <= 1.0f)
-            FoVy = 1.0f;
-        if (FoVy >= 90.0f)
-            FoVy = 90.0f;
-    }
-
-private:
-    void ProcessMouseMovement(float xOffset, float yOffset, bool constrainPitch = true) {
-        yaw += xOffset;
-        pitch += yOffset;
-
-        //std::cout << "yaw = " << yaw << std::endl;
-        //std::cout << "pitch = " << pitch << std::endl;
-
-        // Avem grijã sã nu ne dãm peste cap
-        if (constrainPitch) {
-            if (pitch > 89.0f)
-                pitch = 89.0f;
-            if (pitch < -89.0f)
-                pitch = -89.0f;
-        }
-
-        // Se modificã vectorii camerei pe baza unghiurilor Euler
-        UpdateCameraVectors();
-    }
-
-    void UpdateCameraVectors() {
-        // Calculate the new forward vector
-        this->forward.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
-        this->forward.y = sin(glm::radians(pitch));
-        this->forward.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
-        this->forward = glm::normalize(this->forward);
-        // Also re-calculate the Right and Up vector
-        right = glm::normalize(glm::cross(forward,
-                                          worldUp));  // Normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
-        up = glm::normalize(glm::cross(right, forward));
-    }
-
-protected:
-    const float cameraSpeedFactor = 2.5f;
-    const float mouseSensitivity = 0.1f;
-
-    // Perspective properties
-    float zNear;
-    float zFar;
-    float FoVy;
-    int width;
-    int height;
-    bool isPerspective;
-
-    glm::vec3 position;
-    glm::vec3 forward;
-    glm::vec3 right;
-    glm::vec3 up;
-    glm::vec3 worldUp;
-
-    // Euler Angles
-    float yaw;
-    float pitch;
-
-    bool bFirstMouseMove = true;
-    float lastX = 0.f, lastY = 0.f;
-};
 
 GLuint VAO, VBO, EBO;
 unsigned int VertexShaderId, FragmentShaderId, ProgramId;
@@ -265,76 +63,15 @@ const GLchar *FragmentShader =
 
 
 void CreateVBO() {
-    //float vertices[] = {
-//   // positions          // colors          // texture coords
-//   -1.0f,  -1.0f, 0.0f,  1.0f, 0.0f, 0.0f,   0.0f, 0.0f,
-//   0.0f,  -1.0f, 1.0f,  0.0f, 1.0f, 0.0f,   0.5f, 0.0f,
-//   1.0f,  -1.0f, 0.0f,  0.0f, 0.0f, 1.0f,   1.0f, 0.0f,
-//   0.0f,   1.0f, 0.0f,  1.0f, 1.0f, 0.0f,   0.5f, 1.0f,
-//   -1.0f,  -1.0f, 0.0f, 1.0f, 0.0f, 0.0f,   0.0f, 0.0f,
-//   0.0f,  -1.0f, 1.0f,  0.0f, 1.0f, 0.0f,   0.5f, 1.0f,
-//   1.0f,  -1.0f, 0.0f,  0.0f, 0.0f, 1.0f,   1.0f, 0.0f,
-//};
-//unsigned int indices[] = {
-//   0, 3, 1,
-//   1, 3, 2,
-//   2, 3, 0,
-//   4, 5, 6
-//};
 
-    GLfloat vertices[] = {
-            // front
-            -1.0, -1.0,  1.0, 1.0f, 0.0f, 0.0f,   0.5f, 0.0f,
-            1.0, -1.0,  1.0,  0.0f, 1.0f, 0.0f,   1.0f, 0.0f,
-            1.0,  1.0,  1.0,  0.0f, 0.0f, 1.0f,   1.0f, 1.0f,
-            -1.0,  1.0,  1.0, 1.0f, 1.0f, 0.0f,   0.0f, 1.0f,
-            // top
-            -1.0,  1.0,  1.0, 1.0f, 0.0f, 0.0f,   0.0f, 0.0f,
-            1.0,  1.0,  1.0,  0.0f, 1.0f, 0.0f,   1.0f, 0.0f,
-            1.0,  1.0, -1.0,  0.0f, 0.0f, 1.0f,   1.0f, 1.0f,
-            -1.0,  1.0, -1.0, 1.0f, 1.0f, 0.0f,   0.0f, 1.0f,
-            // back
-            1.0, -1.0, -1.0,  1.0f, 0.0f, 0.0f,   0.0f, 0.0f,
-            -1.0, -1.0, -1.0, 0.0f, 1.0f, 0.0f,   1.0f, 0.0f,
-            -1.0,  1.0, -1.0, 0.0f, 0.0f, 1.0f,   1.0f, 1.0f,
-            1.0,  1.0, -1.0,  1.0f, 1.0f, 0.0f,   0.0f, 1.0f,
-            // bottom
-            -1.0, -1.0, -1.0, 1.0f, 0.0f, 0.0f,   0.0f, 0.0f,
-            1.0, -1.0, -1.0,  0.0f, 1.0f, 0.0f,   1.0f, 0.0f,
-            1.0, -1.0,  1.0,  0.0f, 0.0f, 1.0f,   1.0f, 1.0f,
-            -1.0, -1.0,  1.0, 1.0f, 1.0f, 0.0f,   0.0f, 1.0f,
-            // left
-            -1.0, -1.0, -1.0, 1.0f, 0.0f, 0.0f,   0.0f, 0.0f,
-            -1.0, -1.0,  1.0, 0.0f, 1.0f, 0.0f,   1.0f, 0.0f,
-            -1.0,  1.0,  1.0, 0.0f, 0.0f, 1.0f,   1.0f, 1.0f,
-            -1.0,  1.0, -1.0, 1.0f, 1.0f, 0.0f,   0.0f, 1.0f,
-            // right
-            1.0, -1.0,  1.0, 1.0f, 0.0f, 0.0f,   0.0f, 0.0f,
-            1.0, -1.0, -1.0, 0.0f, 1.0f, 0.0f,   1.0f, 0.0f,
-            1.0,  1.0, -1.0, 0.0f, 0.0f, 1.0f,   1.0f, 1.0f,
-            1.0,  1.0,  1.0, 1.0f, 1.0f, 0.0f,   0.0f, 1.0f,
-    };
+    MyMesh a = GetMesh();
+    GLfloat my_color[72];
+    for (int i = 0; i < 72; i+=3) {
+        my_color[i]=1.0f;
+        my_color[i+1]=1.0f;
+        my_color[i+2]=1.0f;
+    }
 
-    unsigned int indices[] = {
-            // front
-            0,  1,  2,
-            2,  3,  0,
-            // top
-            4,  5,  6,
-            6,  7,  4,
-            // back
-            8,  9, 10,
-            10, 11,  8,
-            // bottom
-            12, 13, 14,
-            14, 15, 12,
-            // left
-            16, 17, 18,
-            18, 19, 16,
-            // right
-            20, 21, 22,
-            22, 23, 20,
-    };
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
     glGenBuffers(1, &EBO);
@@ -342,16 +79,16 @@ void CreateVBO() {
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, a.vertices.size(), a.vertices.data(), GL_STATIC_DRAW);
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, a.v_indices.size(), a.v_indices.data(), GL_STATIC_DRAW);
 
     // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *) 0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void *) 0);
     glEnableVertexAttribArray(0);
     // color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *) (3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (void *) (3 * sizeof(float)));
     glEnableVertexAttribArray(1);
     // texture coord attribute
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *) (6 * sizeof(float)));
@@ -435,7 +172,7 @@ void CreateTextures(const std::string &strExePath) {
     // load image, create texture and generate mipmaps
     int width, height, nrChannels;
     stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
-    unsigned char *data = stbi_load((strExePath + "Stones.jpg").c_str(), &width, &height, &nrChannels, 0);
+    unsigned char *data = stbi_load("Stones.jpg", &width, &height, &nrChannels, 0);
     if (data) {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
@@ -454,7 +191,8 @@ void CreateTextures(const std::string &strExePath) {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     // load image, create texture and generate mipmaps
-    data = stbi_load((strExePath + "Bricks.jpg").c_str(), &width, &height, &nrChannels, 0);
+    std::cout << strExePath;
+    data = stbi_load("Bricks.jpg", &width, &height, &nrChannels, 0);
     if (data) {
         // note that the awesomeface.png has transparency and thus an alpha channel, so make sure to tell OpenGL the data type is of GL_RGBA
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
@@ -495,14 +233,14 @@ void RenderCube() {
 void RenderFunction() {
     glm::vec3 cubePositions[] = {
             glm::vec3(0.0f, 0.0f, 0.0f),
-            glm::vec3(-5.0f, 5.0f, 5.0f),
-            glm::vec3(-5.0f, -5.0f, 5.0f),
-            glm::vec3(5.0f, -5.0f, 5.0f),
-            glm::vec3(5.0f, 5.0f, 5.0f),
-            glm::vec3(-5.0f, 5.0f, -5.0f),
-            glm::vec3(-5.0f, -5.0f, -5.0f),
-            glm::vec3(5.0f, -5.0f, -5.0f),
-            glm::vec3(5.0f, 5.0f, -5.0f),
+    //        glm::vec3(-5.0f, 5.0f, 5.0f),
+    //        glm::vec3(-5.0f, -5.0f, 5.0f),
+    //        glm::vec3(5.0f, -5.0f, 5.0f),
+    //        glm::vec3(5.0f, 5.0f, 5.0f),
+    //        glm::vec3(-5.0f, 5.0f, -5.0f),
+    //        glm::vec3(-5.0f, -5.0f, -5.0f),
+    //        glm::vec3(5.0f, -5.0f, -5.0f),
+    //        glm::vec3(5.0f, 5.0f, -5.0f),
     };
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
